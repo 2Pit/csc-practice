989935600b3776c942999d291e5580a2030be3c8
pbogdano <Petr.Bogdanov@t-systems.com>
2019-03-15 15:05:15 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.10"
+}
+
+group = "com.me"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+    compile("org.eclipse.mylyn.github:org.eclipse.egit.github.core:2.1.5")
+    compile("org.jetbrains.exposed:exposed:0.13.2")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
